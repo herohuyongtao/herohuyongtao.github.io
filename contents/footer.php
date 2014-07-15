8d4c64255a1dae6d72ffbc4e357bdbd1735e20dc
tommyhu7506 <ythu@cs.hku.hk>
2014-07-15 20:57:51 +0800

    extract footer part to separate files

---
+++ contents/footer.php
@@ -0,0 +1,14 @@
+<footer id="content-info" class="container" role="contentinfo">
+	<table width="100%">
+		<tr>
+			<td>
+				<p class="copy"><small>&copy; 2014 Yongtao Hu</small></p>
+			</td>
+			<td style="text-align:right">
+				<a href="http://SiteStates.com" title="Site access statistics">
+					<img src="http://SiteStates.com/show/image/29517.jpg" border="0" />
+				</a>
+			</td>
+		</tr>
+	</table>
+</footer>
